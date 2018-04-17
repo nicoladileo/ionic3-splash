5b8f0f4589252d378ce0161d1c735ad7c03696d4
Nicola Dileo <ndileo@latraccia.it>
2018-04-17 13:17:35 +0200

    Commit of application

---
+++ README.md
@@ -0,0 +1,12 @@
+## ionic3-splash
+#### This ionic 3 project covers usage of custom splash screen and image viewer 
+###### Features:
+- Custom splash screen
+- Animation using Bounce.js
+- Image viewer plugin with zoom
+
+## Thanks to 
+##### [Coursetro](https://www.youtube.com/watch?v=_DdqcjmHOHQ)
+##### [Riron](https://github.com/Riron/ionic-img-viewer)
+
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  tabBarElement: any;
+  splash = true;
+  images = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg'];
   constructor(public navCtrl: NavController) {
+    this.tabBarElement = document.querySelector('.tabbar');
+  }
 
+  ionViewDidLoad() {
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+      this.tabBarElement.style.display = 'flex';
+    }, 4000);
   }
 
 }
